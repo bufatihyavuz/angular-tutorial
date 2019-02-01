da1c26be6835d66b613fa955e5546d14c15bedfd
fatih yavuz <bu.fatih.yavuz@gmail.com>
2019-02-01 17:18:32 +0300

    Two-Way-Databinding Bölüm 2, Ders 30

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = '';
+  serverName = 'TestServer';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -23,7 +23,7 @@ export class ServersComponent implements OnInit {
   }
 
   onUpdateServerName(event: Event) {
-    console.log(event);
+    // console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
